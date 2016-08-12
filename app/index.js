906021276aaf785ded4757c357d94046baf34813
Manav Sehgal <sehgal.manav@gmail.com>
2016-08-12 07:46:46 +0530

    c01 branch close

--- app/index.js
+++ app/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <article className="pa3">
-    <h1>App running in {process.env.NODE_ENV} mode</h1>
-    <p className="measure">
-      This app uses React, Webpack, PostCSS, Tachyon, PostCSS, and Babel,
-      to deliver a modern development pipeline.
-    </p>
-  </article>,
+  <h1>App running in {process.env.NODE_ENV} mode</h1>,
   document.getElementById('app')
 );
